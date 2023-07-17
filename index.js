c9db2b0e80dfce4a57ee098e726a908e823ddec5
ChaseTeague <teaguechase@yahoo.com>
2023-07-17 01:15:13 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gift) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        
+    }
+    return gifts;
+}
+wrapGifts(gifts); 
+
+function writeCards(name, event) {
+    let message = [];
+    for (let i = 0; i < name.length; i++) {
+        message.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return message;
+}
+
+function countDown(number) {
+    while(number >= 0) {
+        console.log(number--);
+    }
+}
